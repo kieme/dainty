415068f9a90f25887fd7eccb58cc405096634f7e
kieme <frits.germs@gmx.net>
2018-10-16 13:01:48 +0200

    Squashed 'library/oops/' changes from 13a27ec..5c7508d
    5c7508d use terminal::t_out
    
    git-subtree-dir: library/oops
    git-subtree-split: 5c7508ddcb1e5a618281a502eed466a3d04ba251

--- dainty_oops.cpp
+++ dainty_oops.cpp
@@ -24,131 +24,128 @@
 
 ******************************************************************************/
 
-#include <stdio.h>
+#include "dainty_named_terminal.h"
 #include "dainty_oops.h"
 
 namespace dainty
 {
 namespace oops
 {
+  using namespace named::terminal;
+
   t_def default_what(t_id) {
-    return t_def(UNRECOVERABLE, P_cstr{"unspecified oops"});
+    return t_def{UNRECOVERABLE, P_cstr{"unspecified oops"}};
   }
 
-  void default_policy(R_info info) {
+  t_void default_policy(R_info info) {
     t_def def(info.what_(info.id_));
     switch (def.category_) {
       case UNRECOVERABLE:
-        printf("policy assert unrecoverable oops = %d, %s\n", info.id_,
-               get(def.string_));
+        t_out{FMT, "policy assert unrecoverable oops = %d, %s\n", info.id_,
+                   get(def.string_)};
         assert_now(P_cstr{"oops->default_policy_assert"});
         break;
       case RECOVERABLE:
-        printf("policy ignore recoverable oops = %d, %s\n", info.id_,
-               get(def.string_));
+        t_out{FMT, "policy ignore recoverable oops = %d, %s\n", info.id_,
+                   get(def.string_)};
         break;
       default:
         break;
     }
   }
 
-  void default_print(R_info info, R_data1 data) {
+  t_void default_print(R_info info, R_data1 data) {
     if (info.what_)
-      printf("oops[tag-%d] = %d, %s\n", data.tag_, info.id_,
-             get(info.what_(info.id_).string_));
+      t_out{FMT, "oops[tag-%d] = %d, %s\n", data.tag_, info.id_,
+                 get(info.what_(info.id_).string_)};
     else
-      printf("oops[tag-%d] = no oops\n", data.tag_);
+      t_out{FMT, "oops[tag-%d] = no oops\n", data.tag_};
   }
 
-  void default_print(R_info info, R_data2 data) {
+  t_void default_print(R_info info, R_data2 data) {
     auto data_file = get(data.file_);
     auto info_file = get(info.file_);
     if (info.what_) {
       if (data_file) {
         if (info_file)
-          printf("oops[%s:%d, tag-%d, depth-%d] = %d, %s, %s:%d\n",
-                 data_file, data.line_, data.tag_, data.depth_, info.id_,
-                 get(info.what_(info.id_).string_), info_file, info.line_);
+          t_out{FMT, "oops[%s:%d, tag-%d, depth-%d] = %d, %s, %s:%d\n",
+                     data_file, data.line_, data.tag_, data.depth_, info.id_,
+                     get(info.what_(info.id_).string_), info_file, info.line_};
         else
-          printf("oops[%s:%d, tag-%d, depth-%d] = %d, %s\n",
-                 data_file, data.line_, data.tag_, data.depth_, info.id_,
-                 get(info.what_(info.id_).string_));
+          t_out{FMT, "oops[%s:%d, tag-%d, depth-%d] = %d, %s\n",
+                     data_file, data.line_, data.tag_, data.depth_, info.id_,
+                     get(info.what_(info.id_).string_)};
       } else {
         if (info_file)
-          printf("oops[tag-%d, depth-%d] = %d, %s, %s:%d\n",
-                 data.tag_, data.depth_, info.id_,
-                 get(info.what_(info.id_).string_), info_file, info.line_);
+          t_out{FMT, "oops[tag-%d, depth-%d] = %d, %s, %s:%d\n",
+                     data.tag_, data.depth_, info.id_,
+                     get(info.what_(info.id_).string_), info_file, info.line_};
         else
-          printf("oops[tag-%d, depth-%d] = %d, %s\n",
-                 data.tag_, data.depth_, info.id_,
-                 get(info.what_(info.id_).string_));
+          t_out{FMT, "oops[tag-%d, depth-%d] = %d, %s\n",
+                     data.tag_, data.depth_, info.id_,
+                     get(info.what_(info.id_).string_)};
       }
     } else {
-      if (data_file) {
-        printf("oops[%s:%d, tag-%d, depth-%d] = no oops\n",
-               data_file, data.line_, data.tag_, data.depth_);
-      } else {
-        printf("oops[tag-%d, depth-%d] = no oops\n", data.tag_, data.depth_);
-      }
+      if (data_file)
+        t_out{FMT, "oops[%s:%d, tag-%d, depth-%d] = no oops\n",
+                   data_file, data.line_, data.tag_, data.depth_};
+      else
+        t_out{FMT, "oops[tag-%d, depth-%d] = no oops\n", data.tag_,
+                   data.depth_};
     }
   }
 
-  void trace_step_in(R_info info, p_what what, P_void context,
-                     R_data1 data) {
-    printf("step_in-> code = %u, data-%p, context = %p\n", info.id_,
-           static_cast<P_void>(&data), context);
-
-    for (t_id id = what(0).next_; id; ) {
-      t_def def = what(id);
-      printf("  -> has code = %u, %s\n", id, get(def.string_));
+  t_void trace_step_in(R_info info, p_what what, P_void context, R_data1 data) {
+    t_out{FMT, "step_in-> code = %u, data-%p, context = %p\n", info.id_,
+               static_cast<P_void>(&data), context};
+    for (auto id = what(0).next_; id; ) {
+      auto def = what(id);
+      t_out{FMT, "  -> has code = %u, %s\n", id, get(def.string_)};
       id = def.next_;
     }
   }
 
-  void trace_step_in(R_info info, p_what what, P_void context,
+  t_void trace_step_in(R_info info, p_what what, P_void context,
                      R_data2 data) {
-    printf("step_in-> code = %u, data-%p, depth = %d, context = %p\n",
-           info.id_, static_cast<P_void>(&data), data.depth_, context);
-
-    for (t_id id = what(0).next_; id; ) {
-      t_def def = what(id);
-      printf("  -> has code = %u, %s\n", id, get(def.string_));
+    t_out{FMT, "step_in-> code = %u, data-%p, depth = %d, context = %p\n",
+               info.id_, static_cast<P_void>(&data), data.depth_, context};
+    for (auto id = what(0).next_; id; ) {
+      auto def = what(id);
+      t_out{FMT, "  -> has code = %u, %s\n", id, get(def.string_)};
       id = def.next_;
     }
   }
 
-  void trace_step_out(R_info info, p_what, P_void context,
-                      R_data1 data) {
-    printf("step_out-> code = %u, data-%p, context = %p\n",
-           info.id_, static_cast<P_void>(&data), context);
+  t_void trace_step_out(R_info info, p_what, P_void context, R_data1 data) {
+    t_out{FMT, "step_out-> code = %u, data-%p, context = %p\n",
+               info.id_, static_cast<P_void>(&data), context};
   }
 
-  void trace_step_out(R_info info, p_what, P_void context,
-                      R_data2 data) {
+  t_void trace_step_out(R_info info, p_what, P_void context, R_data2 data) {
     auto data_file = get(data.file_);
     if (data_file)
-      printf("step_out-> code = %u, data-%p, depth = %d, context = %p, "
-             "file = %s, line = %d\n", info.id_, static_cast<P_void>(&data),
-             data.depth_, context, data_file, data.line_);
+      t_out{FMT, "step_out-> code = %u, data-%p, depth = %d, context = %p, "
+                 "file = %s, line = %d\n", info.id_, static_cast<P_void>(&data),
+                 data.depth_, context, data_file, data.line_};
     else
-      printf("step_out-> code = %u, data-%p, depth = %d, context = %p\n",
-             info.id_, static_cast<P_void>(&data), data.depth_, context);
+      t_out{FMT, "step_out-> code = %u, data-%p, depth = %d, context = %p\n",
+                 info.id_, static_cast<P_void>(&data), data.depth_, context};
   }
 
-  void trace_step_do(R_info info, p_what, P_void context, R_data1 data) {
-    printf("step_do-> code = %u, data-%p, context = %p\n", info.id_,
-           static_cast<P_void>(&data), context);
+  t_void trace_step_do(R_info info, p_what, P_void context, R_data1 data) {
+    t_out{FMT, "step_do-> code = %u, data-%p, context = %p\n", info.id_,
+               static_cast<P_void>(&data), context};
   }
 
-  void trace_step_do(R_info info, p_what, P_void context, R_data2 data) {
+  t_void trace_step_do(R_info info, p_what, P_void context, R_data2 data) {
     auto data_file = get(data.file_);
     if (data_file)
-      printf("step_do-> code = %u, data-%p, depth = %d, context = %p, "
-             "file = %s, line = %d\n", info.id_, static_cast<P_void>(&data),
-             data.depth_, context, data_file, data.line_);
+      t_out{FMT, "step_do-> code = %u, data-%p, depth = %d, context = %p, "
+                 "file = %s, line = %d\n", info.id_, static_cast<P_void>(&data),
+                 data.depth_, context, data_file, data.line_};
     else
-      printf("step_do-> code = %u, data-%p, depth = %d, context = %p\n",
-             info.id_, static_cast<P_void>(&data), data.depth_, context);
+      t_out{FMT, "step_do-> code = %u, data-%p, depth = %d, context = %p\n",
+                 info.id_, static_cast<P_void>(&data), data.depth_, context};
   }
 }
 }
