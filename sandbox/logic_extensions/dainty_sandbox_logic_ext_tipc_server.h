1ee11901aa99107219ebc5f5621d769cf352a451
kieme <frits.germs@gmx.net>
2019-03-07 22:08:11 +0100

    add tipc extension. very important for daint::messaging

---
+++ sandbox/logic_extensions/dainty_sandbox_logic_ext_tipc_server.h
@@ -0,0 +1,79 @@
+/******************************************************************************
+
+MIT License
+
+Copyright (c) 2018 frits.germs@gmx.net
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+******************************************************************************/
+
+#ifndef _DAINTY_SANDBOX_LOGIC_EXT_TIPC_SERVER_H_
+#define _DAINTY_SANDBOX_LOGIC_EXT_TIPC_SERVER_H_
+
+#include "dainty_sandbox_logic_ext.h"
+#include "dainty_sandbox_logic_ext_tipc_server_api.h"
+#include "dainty_sandbox_logic_ext_tipc_server_notify.h"
+
+namespace dainty
+{
+namespace sandbox
+{
+namespace logic_tipc_server_ext
+{
+///////////////////////////////////////////////////////////////////////////////
+
+  class t_logic_tipc_server_ext : public t_logic_tipc_server_ext_api,
+                                  public t_logic_tipc_server_ext_notify {
+  public:
+    t_logic_tipc_server_ext(t_err, r_logic) noexcept;
+
+    // use api
+    // provide notify_
+
+  private:
+    using r_ext_ = t_prefix<t_logic_tipc_server_ext>::r_;
+
+    class t_impl_ final : public t_logic_ext {
+    public:
+      t_impl_(t_err, r_logic, r_ext_) noexcept;
+
+    private:
+      t_void notify_start   (t_err)  noexcept override final;
+      t_void notify_cleanup ()       noexcept override final;
+      t_void notify_wakeup  (t_msec) noexcept override final;
+      t_void notify_complete()       noexcept override final;
+      t_void notify_timeout (t_timer_id,
+                             R_timer_params)   noexcept override final;
+      t_void notify_fdevent (t_fdevent_id,
+                             R_fdevent_params) noexcept override final;
+
+    private:
+      r_ext_ ext_;
+    };
+
+    t_impl_ impl_;
+  };
+
+///////////////////////////////////////////////////////////////////////////////
+}
+}
+}
+
+#endif
