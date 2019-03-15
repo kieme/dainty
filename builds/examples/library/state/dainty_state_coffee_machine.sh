8417b986b98c770bfcf27bf4219f7e4e6ce19e23
kieme <frits.germs@gmx.net>
2019-03-15 09:32:01 +0100

    enable statemachine debugging in the example

--- builds/examples/library/state/dainty_state_coffee_machine.sh
+++ builds/examples/library/state/dainty_state_coffee_machine.sh
@@ -1,6 +1,9 @@
 export DAINTY_ROOT=../../../..
 g++ -Wall -o coffee_machine \
     $DAINTY_ROOT/examples/library/state/dainty_state_coffee_machine.cpp \
+    $DAINTY_ROOT/library/named/dainty_named_string_impl.cpp \
+    $DAINTY_ROOT/library/named/dainty_named_assert.cpp \
+    $DAINTY_ROOT/library/named/dainty_named_terminal.cpp \
   -I$DAINTY_ROOT/library/named \
   -I$DAINTY_ROOT/library/state \
-    -std=c++17 -lpthread -O2 -ggdb3
+    -std=c++17 -lpthread -O0 -ggdb3 -DCOFFEE_DEBUG
