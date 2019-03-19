f48d132f63ccd8550cb3822e28e3ef59fbde474d
kieme <frits.germs@gmx.net>
2019-03-19 22:15:34 +0100

    fix the simple_mechanis example

--- examples/library/state/dainty_state_see_simple_mechanics.cpp
+++ examples/library/state/dainty_state_see_simple_mechanics.cpp
@@ -3,100 +3,111 @@
 
 #include "dainty_state.h"
 
-using namespace dainty;
-using named::t_void;
+using dainty::named::t_void;
+using dainty::named::t_prefix;
 
 ////////////////////////// design phase ////////////////////////////////
 
 // which states are required
-enum t_states { STATE1 = 0, STATE2, STATE3, STATE4, STOP };
+enum t_state_id { STATE1 = 0, STATE2, STATE3, STATE4, STOP };
 
 // what data must be shared among them and the statemachine
 struct t_data { };
+using  r_data = t_prefix<t_data>::r_;
 
 // what interface of functions are required and must be implemented
 struct t_interface {
   virtual ~t_interface() { }
-  virtual t_states foo() = 0;
+  virtual t_state_id foo() = 0;
 };
 
 //////////////// choose statemachine framework  ////////////////////////
 
-using t_traits       = state::t_traits<t_states, t_data, t_interface>;
-using t_state        = t_traits::t_state;
-using t_statemachine = t_traits::t_statemachine;
+using t_traits_       = dainty::state::t_traits<t_state_id, t_data, t_interface>;
+using t_state_        = t_traits_::t_state;
+using t_statemachine_ = t_traits_::t_statemachine;
 
 //////////////////// implementation phase //////////////////////////////
 
-struct t_state1 final : t_state {
-  t_state1(t_data& d) : t_state(STATE1, d) { }
-  virtual t_sid  entry_point() override;
-  virtual t_void exit_point()  override;
-  virtual t_sid  foo()         override;
+struct t_state1 final : t_state_ {
+  t_state1(r_data data) : t_state_{STATE1, data} { }
+
+  t_state_id entry_point() override;
+  t_void     exit_point () override;
+  t_state_id foo        () override;
 };
 
-struct t_state2 final : t_state {
-  t_state2(t_data& d) : t_state(STATE2, d) { }
-  virtual t_sid  entry_point() override;
-  virtual t_void exit_point()  override;
-  virtual t_sid  foo()         override;
+struct t_state2 final : t_state_ {
+  t_state2(r_data data) : t_state_{STATE2, data} { }
+
+  t_state_id entry_point() override;
+  t_void     exit_point () override;
+  t_state_id foo        () override;
 };
 
-struct t_state3 final : t_state {
-  t_state3(t_data& d) : t_state(STATE3, d) { }
-  virtual t_sid  entry_point() override;
-  virtual t_void exit_point()  override;
-  virtual t_sid  foo()         override;
+struct t_state3 final : t_state_ {
+  t_state3(r_data data) : t_state_{STATE3, data} { }
+  t_state_id entry_point() override;
+  t_void     exit_point () override;
+  t_state_id foo        () override;
 };
 
-struct t_state4 final : t_state {
-  t_state4(t_data& d) : t_state(STATE4, d) { }
-  virtual t_sid  entry_point() override;
-  virtual t_void exit_point()  override;
-  virtual t_sid  foo()         override;
+struct t_state4 final : t_state_ {
+  t_state4(r_data data) : t_state_{STATE4, data} { }
+  t_state_id entry_point() override;
+  t_void     exit_point () override;
+  t_state_id foo        () override;
 };
 
-struct t_sm : t_statemachine {
-  t_sm(t_data& d) : t_statemachine(STOP, d), s1_(d), s2_(d), s3_(d), s4_(d) {
+class t_statemachine final : public t_statemachine_ {
+public:
+  t_statemachine(r_data data)
+    : t_statemachine_{STOP, data}, s1_{data}, s2_{data}, s3_{data}, s4_{data} {
     states_[STATE1] = &s1_;
     states_[STATE2] = &s2_;
     states_[STATE3] = &s3_;
     states_[STATE4] = &s4_;
+
     assert(start(STATE1) == STATE1);
   }
 
-  ~t_sm()        { stop(); }
+ ~t_statemachine() {
+    stop();
+  }
 
-  t_void reset() { assert(restart(STATE1) == STATE1); }
+  t_void restart() {
+    assert(t_statemachine_::restart(STATE1) == STATE1);
+  }
 
-  virtual t_states foo          ()              override {
+  t_state_id foo() override {
     return do_transition(get_current()->foo());
   }
 
-  virtual t_states initial_point(t_states s)    override;
-  virtual t_void   final_point  ()              override;
+private:
+  t_state_id initial_point(t_state_id) override;
+  t_void   final_point  ()             override;
 
-  virtual p_state  get_state    (t_sid i)       override { return states_[i]; }
-  virtual P_state  get_state    (t_sid i) const override { return states_[i]; }
+  p_state get_state(t_state_id id)       noexcept override { return states_[id]; }
+  P_state get_state(t_state_id id) const noexcept override { return states_[id]; }
 
   t_state1 s1_;
   t_state2 s2_;
   t_state3 s3_;
   t_state4 s4_;
-  t_state* states_[STOP];
+  p_state states_[STOP];
 };
 
 // function uses the statemachine
 class t_function {
 public:
-  t_function() : sm_(data_)   { }
+  t_function() : statemachine_(data_)     { }
 
-  t_void foo()   { sm_.foo();   }
-  t_void reset() { sm_.reset(); }
+  t_void foo()   { statemachine_.foo();     }
+  t_void reset() { statemachine_.restart(); }
 
 private:
-  t_data data_;
-  t_sm   sm_;
+  t_data         data_;
+  t_statemachine statemachine_;
 };
 
 // execution
@@ -113,7 +124,7 @@ int main() {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-t_state1::t_sid t_state1::entry_point() {
+t_state1::t_state_id t_state1::entry_point() {
   std::cout << "state1::entry_point" << std::endl;
   return no_transition();
 }
@@ -122,12 +133,12 @@ t_state1::t_void t_state1::exit_point() {
   std::cout << "state1::exit_point"  << std::endl;
 }
 
-t_state1::t_sid t_state1::foo() {
+t_state1::t_state_id t_state1::foo() {
   std::cout << "state1::foo" << std::endl;
   return request_transition(STATE2);
 }
 
-t_state2::t_sid t_state2::entry_point() {
+t_state2::t_state_id t_state2::entry_point() {
   std::cout << "state2::entry_point" << std::endl;
   return no_transition();
 }
@@ -136,12 +147,12 @@ t_state2::t_void t_state2::exit_point() {
   std::cout << "state2::exit_point"  << std::endl;
 }
 
-t_state2::t_sid t_state2::foo() {
+t_state2::t_state_id t_state2::foo() {
   std::cout << "state2::foo" << std::endl;
   return request_transition(STATE3);
 }
 
-t_state3::t_sid t_state3::entry_point() {
+t_state3::t_state_id t_state3::entry_point() {
   std::cout << "state3::entry_point" << std::endl;
   return no_transition();
 }
@@ -150,12 +161,12 @@ t_state3::t_void t_state3::exit_point() {
   std::cout << "state3::exit_point" << std::endl;
 }
 
-t_state3::t_sid t_state3::foo() {
+t_state3::t_state_id t_state3::foo() {
   std::cout << "state3::foo" << std::endl;
   return request_transition(STATE4);
 }
 
-t_state4::t_sid t_state4::entry_point() {
+t_state4::t_state_id t_state4::entry_point() {
   std::cout << "state4::entry_point" << std::endl;
   return no_transition();
 }
@@ -164,17 +175,17 @@ t_state4::t_void t_state4::exit_point() {
   std::cout << "state4::exit_point" << std::endl;
 }
 
-t_state4::t_sid t_state4::foo() {
+t_state4::t_state_id t_state4::foo() {
   std::cout << "state4::foo" << std::endl;
   return request_transition(STATE1);
 }
 
-t_states t_sm::initial_point(t_states s) {
+t_state_id t_statemachine::initial_point(t_state_id id) {
   std::cout << "initial action" << std::endl;
-  return s;
+  return id;
 }
 
-t_sm::t_void t_sm::final_point() {
+t_void t_statemachine::final_point() {
   std::cout << "final action" << std::endl;
 }
 
