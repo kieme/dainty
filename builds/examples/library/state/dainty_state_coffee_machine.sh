fd2a1a5ad3bd356d4d31abe61716542a42f09ae7
kieme <frits.germs@gmx.net>
2019-03-19 20:55:31 +0100

    enable range checking

--- builds/examples/library/state/dainty_state_coffee_machine.sh
+++ builds/examples/library/state/dainty_state_coffee_machine.sh
@@ -4,6 +4,7 @@ g++ -Wall -o coffee_machine \
     $DAINTY_ROOT/library/named/dainty_named_string_impl.cpp \
     $DAINTY_ROOT/library/named/dainty_named_assert.cpp \
     $DAINTY_ROOT/library/named/dainty_named_terminal.cpp \
+    $DAINTY_ROOT/library/named/dainty_named_range.cpp \
   -I$DAINTY_ROOT/library/named \
   -I$DAINTY_ROOT/library/state \
-    -std=c++17 -lpthread -O0 -ggdb3 -DCOFFEE_DEBUG
+    -std=c++17 -lpthread -O0 -ggdb3 -DCOFFEE_DEBUG -DDAINTY_NAMED_RANGE_CHECK
